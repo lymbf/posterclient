3de08f8580ce85e7f8e0c75f9c2a031537439f4c
lymbf <paw.czekaj@gmail.com>
2025-05-26 01:43:56 +0200

    added success copy icons

--- components/facebookLoginButton.tsx
+++ components/facebookLoginButton.tsx
@@ -4,6 +4,7 @@ import {cn} from "@/lib/utils";
 import FacebookLogin, {SuccessResponse} from "@greatsumini/react-facebook-login";
 import {useState} from "react";
 import CopyIcon from "@/components/ui/copyIcon";
+import SuccessIcon from "@/components/ui/successIcon";
 
 interface Room {
     roomID: string,
@@ -14,6 +15,9 @@ export default function FacebookLoginButton({className}: { className?: string })
     const [message, setMessage] = useState<{ text: string, severity: 'error' | 'success' }>()
     const [token, setToken] = useState<string>()
     const [userId, setUserId] = useState<string>()
+    const [firstCopy, setFirstCopy] = useState<boolean>(false)
+    const [secondCopy, setSecondCopy] = useState<boolean>(false)
+
     const [rooms, setRooms] = useState<Room[]>();
     const handleSuccess = (res: SuccessResponse) => {
 
@@ -49,13 +53,17 @@ export default function FacebookLoginButton({className}: { className?: string })
                            ' mt-[10px] p-2 pl-4 pr-4 h-10 m-0 w-[400px]',
                            'text-center')}
                 />
-                <CopyIcon
+                {token && !firstCopy &&  <CopyIcon
                     className={cn('w-[25px] h-[25px] fill-muted-foreground absolute top-[50%] translate-y-[-50%] right-[10px]',
                         'hover:fill-foreground hover:cursor-pointer transition-all duration-200')}
-                    onClick = {()=>{
-                        token && navigator.clipboard.writeText(token).then(r=>{})
+                    onClick={() => {
+                        token && navigator.clipboard.writeText(token).then(r => {
+                            setFirstCopy(true)
+                        })
                     }}
-                />
+
+                />}
+                {firstCopy && <SuccessIcon className={'w-[25px] h-[25px] fill-green-500 absolute top-[50%] translate-y-[-50%] right-[10px]'}/>}
             </div>
             <label className={'mt-[40px] uppercase font-bold text-[14px] text-foreground'}>
                 User_ID
@@ -66,13 +74,16 @@ export default function FacebookLoginButton({className}: { className?: string })
                            ' mt-[10px] p-2 pl-4 pr-4 h-10 w-[400px] m-0',
                            'text-center')}
                 />
-                <CopyIcon
+                {userId && !secondCopy && <CopyIcon
                     className={cn('w-[25px] h-[25px] fill-muted-foreground absolute top-[50%] translate-y-[-50%] right-[10px]',
                         'hover:fill-foreground hover:cursor-pointer transition-all duration-200')}
-                    onClick = {()=>{
-                       userId && navigator.clipboard.writeText(userId).then(r=>{console.log('r: ', r)})
+                    onClick={() => {
+                        userId && navigator.clipboard.writeText(userId).then(r => {
+                            setSecondCopy(true)
+                        })
                     }}
-                />
+                />}
+                {secondCopy && <SuccessIcon className={'w-[25px] h-[25px] fill-muted-foreground absolute top-[50%] translate-y-[-50%] right-[10px]'}/>}
             </div>
 
         </div>
