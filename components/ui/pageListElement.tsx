dfd2f0302cdcaede3b287ecc5786cd75552f2e61
lymbf <paw.czekaj@gmail.com>
2025-05-26 13:04:38 +0200

    added margin

--- components/ui/pageListElement.tsx
+++ components/ui/pageListElement.tsx
@@ -15,7 +15,7 @@ export default function PageListElement({className, name, pageId}:{className?:st
     return(
         <div
             className={cn('relative flex flex-row justify-center items-center w-[400px] h-[40px] border-muted border-solid border-[1px]',
-                'p-2 px-4 font-normal text-[12px] text-foreground rounded-md', className)}>
+                'p-2 px-4 font-normal text-[12px] text-foreground rounded-md mb-[10px]', className)}>
             <span className={'font-bold mr-[5px]'}>Name: </span>{` ${name}     `} <span className={'font-bold ml-[30px] mr-[5px]'}>pageID:</span>{` ${pageId}     `}
             {!copied && <CopyIcon
                 className={cn('w-[14px] h-[14px] fill-muted-foreground absolute top-[50%] translate-y-[-50%] right-[10px]',
